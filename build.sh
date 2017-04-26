af1248eddd688de6e0f96fbc4cb065e3681f79cf
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-04-26 16:50:02 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This line MUST be present in all scripts executed by cron!
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+## PARAMETERS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+DATE=`date +%d/%m/%Y-%H:%M:%S`
+
+if [ $USER = "root" ] ; then
+  if [ ! -f /var/run/docker.sock ]; then
+    read -p "Docker is not installed, do you wan't to install it now ? y/n" installDocker
+    case $installDocker in
+    "y")
+    apt install docker
+    ;;
+    "n")
+    echo -e "We'll not install Docker"
+    ;;
+    *)
+    exit 0;
+  fi
+else
+
+fi
