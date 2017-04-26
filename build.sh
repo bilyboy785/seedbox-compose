2db8c174b06f0ba0b9ee420b5c453a6898876e59
Martin Bouillaud <martinbouillaud@macbook-pro-de-martin.home>
2017-04-26 20:41:27 +0200

    Add functions file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source functions.sh
 # This line MUST be present in all scripts executed by cron!
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
@@ -8,23 +8,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 DATE=`date +%d/%m/%Y-%H:%M:%S`
+DOCKERLIST="/etc/apt/sources.list.d/docker.list"
 
 if [ $USER = "root" ] ; then
-  dpkg-query -l docker
-  echo $?
-  if [ $? != 0 ]; then
-    read -p "Docker is not installed, do you wan't to install it now ? (y/n) : " installDocker
-      case $installDocker in
-      "y")
-        apt install docker
-       ;;
-      "n")
-        echo "We'll not install Docker"
-      ;;
-      *)
-        exit 0;
-      ;;
-      esac
-  fi
-  echo "Début !"
+  install_docker
+  instal_letsencrypt
 fi
---
+++ functions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+function install_docker() {
+	dpkg-query -l docker >> /dev/null
+  	if [ $? != 0 ]; then
+		read -p "Docker is not installed, do you wan't to install it now ? (y/n) : " installDocker
+		case $installDocker in
+		    "y")
+		    	echo "deb https://apt.dockerproject.org/repo debian-jessie main" > $DOCKERLIST
+		        apt update
+		        apt install docker docker-engine docker-compose
+		    ;;
+		    "n")
+		        echo "We'll not install Docker"
+		        echo "$RED Exiting$NC"
+		    ;;
+		    *)
+		        exit 0;
+		    ;;
+		esac
+	fi
+}
+
+function install_letsencrypt() {
+	if [! -d "/etc/letsencrypt" ]; then
+		read -p "Lets'Encrypt is not installed. Do you plan to generate certificates ? (y/n) : " installLetsencrypt
+		case $installLetsencrypt in
+		    "y")
+		    	apt install git-core
+		    	git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+		   		cd /opt/letsencrypt
+		    ;;
+		    "n")
+		        echo "We'll not install LetsEncrypt"
+		        echo "$RED Exiting$NC"
+		    ;;
+		    *)
+		        exit 0;
+		    ;;
+		esac
+	else
+		echo "letsencrypt ok"
+	fi
+}
