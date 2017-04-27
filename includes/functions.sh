d0115cee443fddab3e0627d4091c79b716c269c1
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-04-27 10:56:38 +0200

    Update functions.sh

--- includes/functions.sh
+++ includes/functions.sh
@@ -15,6 +15,7 @@ function intro() {
 }
 
 function install_docker() {
+	echo "## DOCKER ##
 	dpkg-query -l docker >> /dev/null
   	if [ $? != 0 ]; then
 		echo "Docker is not installed, it will be installed !"
@@ -29,6 +30,7 @@ function install_docker() {
 }
 
 function install_letsencrypt() {
+	echo "## LETS ENCRYPT ##
 	if [ ! -d "/etc/letsencrypt" ]; then
 		read -p "Lets'Encrypt is not installed. Do you plan to generate certificates ? (y/n) : " installLetsencrypt
 		apt install git-core
@@ -41,15 +43,17 @@ function install_letsencrypt() {
 }
 
 function choose_services() {
+	echo "## SERVICES ##
 	echo "Nginx, MariaDB, Nextcloud, RuTorrent/rTorrent, Sonarr, Radarr, Jackett and Docker WebUI will be installed by default !"
 	echo "Choose wich services you want to add : "
-	read -p "		Plex and PlexPy ? (y/n) : " PLEXINSTALL
-	read -p "		ZeroBin ? (y/n) : " ZEROBININSTALL
-	read -p "		Lufi & Lutim ? (y/n) : " LUFILUTIMINSTALL
+	read -p "	Plex and PlexPy ? (y/n) : " PLEXINSTALL
+	read -p "	ZeroBin ? (y/n) : " ZEROBININSTALL
+	read -p "	Lufi & Lutim ? (y/n) : " LUFILUTIMINSTALL
 	echo ""
 }
 
 function define_parameters() {
+	echo "## PARAMETERS ##
 	read -p "Please enter user ID you want to run dockers : " USERID
 	read -p "Please enter group ID you want to run dockers : " GRPID
 	TIMEZONEDEF=$(cat /etc/timezone)
