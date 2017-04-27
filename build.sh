c0a413b2cd34498ab9917242393b25b9c3133c64
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-04-27 16:47:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ RED='\e[0;31m'
 GREEN='\033[0;32m'
 BLUE='\e[0;36m'
 YELLOW='\e[0;33m'
+BWHITE='\e[1;37m'
 NC='\033[0m'
 DATE=`date +%d/%m/%Y-%H:%M:%S`
 DOCKERLIST="/etc/apt/sources.list.d/docker.list"
