7be65733563a780c3b3cfeb59238eef0edab7795
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-04-27 10:50:25 +0200

    Update functions.sh

--- includes/functions.sh
+++ includes/functions.sh
@@ -22,16 +22,6 @@ function install_docker() {
 		apt update
 		apt install docker docker-engine docker-compose
 		echo ""
-		    ;;
-		    "n")
-		        echo "We'll not install Docker"
-		        echo "Exiting"
-			echo ""
-		    ;;
-		    *)
-		        exit 0;
-		    ;;
-		esac
 	else
 		echo "Docker is already installed !"
 		echo ""
@@ -41,20 +31,9 @@ function install_docker() {
 function install_letsencrypt() {
 	if [ ! -d "/etc/letsencrypt" ]; then
 		read -p "Lets'Encrypt is not installed. Do you plan to generate certificates ? (y/n) : " installLetsencrypt
-		case $installLetsencrypt in
-		    "y")
-		    	apt install git-core
-		    	git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-		    ;;
-		    "n")
-		        echo "We'll not install LetsEncrypt"
-		        echo "Exiting"
-			echo ""
-		    ;;
-		    *)
-		        exit 0;
-		    ;;
-		esac
+		apt install git-core
+		git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+		echo ""
 	else
 		echo "Let's Encrypt is already installed !"
 		echo ""
