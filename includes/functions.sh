2313052b5c45491e85739e6ce5266d6c7903ecd5
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-02 11:04:10 +0200

    Update functions.sh

--- includes/functions.sh
+++ includes/functions.sh
@@ -121,8 +121,8 @@ function install_docker() {
 
 function install_letsencrypt() {
 	echo -e "${BLUE}## LETS ENCRYPT ##${NC}"
-	LEDIR="/etc/letsencrypt"
-	if [ ! -f "$LEDIR" ]; then
+	LEDIR="/opt/letsencrypt"
+	if [[ ! -d "$LEDIR" ]]; then
 		echo "	* Lets'Encrypt is not installed. It will be installed"
 		apt install -y git-core > /dev/null 2>&1
 		git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
