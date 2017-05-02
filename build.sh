f884da8afe9ba0c126dc782eedb3a24351c6d20d
Martin Bouillaud <martinbouillaud@macbook-pro-de-martin.home>
2017-05-02 17:52:29 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,52 +21,52 @@ SERVICES="includes/services"
 FILEPORTPATH="/etc/seedboxcompose/ports.pt"
 
 if [ $USER = "root" ] ; then
-  ## Display script infos 
-  intro
-  ## Check option for script lauching
-  script_option
-  case $SCRIPT in
-	"INSTALL")
-	    if [[ -f "/etc/seedboxcompose/seedboxcompose.txt" ]]; then
-	    	## Upgrading system
-		upgrade_system
-		    ## Check for docker on system
-		    install_docker
-		    ## Installing base packages
-		    base_packages
-		    ## Check for LetsEncrypt packages on system
-		    install_letsencrypt
-		    ## Choose wich services install
-		    choose_services
-		    ## Update docker-compose file
-		    install_services
-		    ## Defines parameters for dockers : password, domains and replace it in docker-compose file
-		    define_parameters
-		    ## Generate dockers apps running in background
-		    docker_compose
-		    ## Create reverse proxy for each apps
-		    create_reverse
-		    ## Validating Htpasswd
-		    valid_htpasswd
-		    ## Write a log file for the next script launching
-		    already_installed
-	    else
-	    	exit 1
-	    fi
-	  ;;
-	"ADDUSER")
-	    add_user_htpasswd	
-	  ;;
-	"ADDDOCKAPP")
-	    echo ""
-	  ;;
-	"RESTARTDOCKER")
-	    restart_docker_apps
-	  ;;
-	"DELETEDOCKERS")
-	    delete_dockers
-	  ;;
-	"BACKUPCONF")
-	    backup_docker_conf
+	## Display script infos 
+	intro
+	## Check option for script lauching
+	script_option
+	case $SCRIPT in
+		"INSTALL")
+	    	if [[ ! -f "/etc/seedboxcompose/seedboxcompose.txt" ]]; then
+		    	## Upgrading system
+				upgrade_system
+			    ## Check for docker on system
+			    install_docker
+			    ## Installing base packages
+			    base_packages
+			    ## Check for LetsEncrypt packages on system
+			    install_letsencrypt
+			    ## Choose wich services install
+			    choose_services
+			    ## Update docker-compose file
+			    install_services
+			    ## Defines parameters for dockers : password, domains and replace it in docker-compose file
+			    define_parameters
+			    ## Generate dockers apps running in background
+			    docker_compose
+			    ## Create reverse proxy for each apps
+			    create_reverse
+			    ## Validating Htpasswd
+			    valid_htpasswd
+			    ## Write a log file for the next script launching
+			    already_installed
+	    	else
+	    		exit 1
+	    	fi
+	  	;;
+		"ADDUSER")
+	    	add_user_htpasswd	
+	  	;;
+		"ADDDOCKAPP")
+	    	echo ""
+	  	;;
+		"RESTARTDOCKER")
+	    	restart_docker_apps
+	  	;;
+		"DELETEDOCKERS")
+	    	delete_dockers
+	  	;;
+		"BACKUPCONF")
+	    	backup_docker_conf
 	esac
 fi
