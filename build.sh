cff522d04fbd14985c05b1ea0a6874539671196d
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-04 14:43:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ CONFDIR="/etc/seedboxcompose"
 DATE=`date +%d/%m/%Y-%H:%M:%S`
 BACKUPDATE=`date +%d-%m-%Y-%H-%M-%S`
 DOCKERLIST="/etc/apt/sources.list.d/docker.list"
+IPADDRESS=$(ip a | grep eth0 | awk '/inet /{print substr($2,1)}' | cut -d\/ -f1)
 FIRSTPORT="5050"
 LASTPORT="8080"
 SERVICES="includes/services"
