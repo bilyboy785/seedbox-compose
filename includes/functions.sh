5220b51578e683a33f8b10fbbccdd0588459ec3b
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-11 10:14:02 +0200

    Update functions.sh

--- includes/functions.sh
+++ includes/functions.sh
@@ -15,7 +15,6 @@ function intro() {
 }
 function script_option() {
 	echo -e "${BLUE}### WELCOME TO SEEDBOX-COMPOSE ###${NC}"
-	conf_dir
 	echo "This script will help you to make a complete seedbox with Rutorrent, Sonarr, Radarr and Jacket, based on Docker !"
 	echo "Choose an option to launch the script (1, 2...) : "
 	echo ""
