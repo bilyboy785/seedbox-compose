eade21640a1c463cf07da8ded7118d7b52941dbf
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-12 11:16:49 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ BACKUPDATE=`date +%d-%m-%Y-%H-%M-%S`
 IPADDRESS=$(ip a | grep eth0 | awk '/inet /{print substr($2,1)}' | cut -d\/ -f1)
 FIRSTPORT="5050"
 LASTPORT="8080"
-UNDERDEV="${RED}### UNDERDEVELOPPEMENT ###${NC}"
 CONFDIR="/etc/seedboxcompose"
 DOCKERLIST="/etc/apt/sources.list.d/docker.list"
 SERVICESAVAILABLE="includes/config/services-available"
@@ -83,7 +82,7 @@ if [ $USER = "root" ] ; then
 			schedule_backup_seedbox
 		;;
 	  	"DELETEHTACCESS")
-			echo -e $UNDERDEV
+			under_developpment
 			#delete_htaccess
 		;;
 		"ADDDOCKAPP")
@@ -96,7 +95,7 @@ if [ $USER = "root" ] ; then
 	    		restart_docker_apps
 	  	;;
 		"DELETEDOCKERS")
-			echo -e $UNDERDEV
+			under_developpment
 	    		#delete_dockers
 	  	;;
 		"BACKUPCONF")
