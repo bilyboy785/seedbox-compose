58ca94602f74b4707356d64ca7cbae05149214f5
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-14 23:34:22 +0200

    Update functions

--- build.sh
+++ build.sh
@@ -40,44 +40,44 @@ if [ $USER = "root" ] ; then
 	case $SCRIPT in
 		"INSTALL")
 	    	if [[ ! -d "/etc/seedboxcompose/" ]]; then
-	  		echo -e "${BLUE}##########################################${NC}"
-	  		echo -e "${BLUE}###    INSTALLING SEEDBOX-COMPOSE      ###${NC}"
-	  		echo -e "${BLUE}##########################################${NC}"
-			conf_dir
-			## Install base packages
-			install_base_packages
-		    ## Checking system version
-			checking_system
-			## Check for docker on system
-			install_docker
-			## Installing Nginx
-			install_nginx
-			## Installing ZSH
-			install_zsh
-			## Check for LetsEncrypt packages on system
-			#install_letsencrypt
-			## Defines parameters for dockers : password, domains and replace it in docker-compose file
-			define_parameters
-			## Choose wich services install
-			choose_services
-			## Update docker-compose file
-			install_services
-			## Generate dockers apps running in background
-			docker_compose
-			## Create reverse proxy for each apps
-			create_reverse
-			## Validating Htpasswd
-			valid_htpasswd
-			## Resuming seedbox-compose installation
-			resume_seedbox
-			## Backup dockers app Configuration
-			#backup_docker_conf
-			schedule_backup_seedbox
-			## Display Teamspeak IDs
-			##access_token_ts
+		  		echo -e "${BLUE}##########################################${NC}"
+		  		echo -e "${BLUE}###    INSTALLING SEEDBOX-COMPOSE      ###${NC}"
+		  		echo -e "${BLUE}##########################################${NC}"
+				conf_dir
+				## Install base packages
+				install_base_packages
+			    ## Checking system version
+				checking_system
+				## Check for docker on system
+				install_docker
+				## Installing Nginx
+				install_nginx
+				## Installing ZSH
+				install_zsh
+				## Check for LetsEncrypt packages on system
+				#install_letsencrypt
+				## Defines parameters for dockers : password, domains and replace it in docker-compose file
+				define_parameters
+				## Choose wich services install
+				choose_services
+				## Update docker-compose file
+				install_services
+				## Generate dockers apps running in background
+				docker_compose
+				## Create reverse proxy for each apps
+				create_reverse
+				## Validating Htpasswd
+				valid_htpasswd
+				## Resuming seedbox-compose installation
+				resume_seedbox
+				## Backup dockers app Configuration
+				#backup_docker_conf
+				schedule_backup_seedbox
+				## Display Teamspeak IDs
+				##access_token_ts
 	    	else
-			clear
-			echo -e " ${RED}--> Seedbox-Compose already installed !${NC}"
+				clear
+				echo -e " ${RED}--> Seedbox-Compose already installed !${NC}"
 	    		script_option
 	    	fi
 	  	;;
