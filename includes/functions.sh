5a77312476ac49aababfd958a876a6a89b02251c
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-14 23:37:17 +0200

    Update menu

--- includes/functions.sh
+++ includes/functions.sh
@@ -66,7 +66,7 @@ function script_option() {
 		;;
 		esac
 	else
-		ACTION=$(whiptail --title "Seedbox-Compose" --menu "Welcome to Seedbox-Compose Script. Please install it first !" 10 60 5 \
+		ACTION=$(whiptail --title "Seedbox-Compose" --menu "Welcome to Seedbox-Compose Script. Please install it first !" 10 60 2 \
 			"1" "Install Seedbox-Compose" 3>&1 1>&2 2>&3)
 		echo ""
 		case $ACTION in
