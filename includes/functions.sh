87d6b7c1e8e6299b29158e3cc2906065236378bd
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-16 11:45:11 +0200

    Update functions

--- includes/functions.sh
+++ includes/functions.sh
@@ -560,31 +560,31 @@ function install_ftp_server() {
 		if (whiptail --title "Use FTP Server" --yesno "Do you want to install FTP server ?" 7 50) then
 			FTPSERVERNAME=$(whiptail --title "FTPServer Name" --inputbox \
 			"Please enter a name for your FTP Server :" 7 50 "SeedBox" 3>&1 1>&2 2>&3)
-			echo -e "	${BWHITE}* Installing proftpd...${NC}"
+			echo -e " ${BWHITE}* Installing proftpd...${NC}"
 			apt-get install proftpd -y
 			checking_errors $?
-			echo -e "	${BWHITE}* Creating configuration file...${NC}"
+			echo -e " ${BWHITE}* Creating configuration file...${NC}"
 			mv "$PROFTPDFOLDER$PROFTPDCONFFILE" "$PROFTPDFOLDER$PROFTPDCONFFILE.bak"
 	 	 	cat "$BASEPROFTPDFILE" >> "$PROFTPDFOLDER$PROFTPDCONFFILE"
 	 	 	sed -i -e "s/ServerName\ \"Debian\"/$FTPSERVERNAME/g" "$PROFTPDFOLDER$PROFTPDCONFFILE"
 	 		checking_errors $?
-	 		echo -e "	${BWHITE}* Restarting service...${NC}"
+	 		echo -e " ${BWHITE}* Restarting service...${NC}"
 	 		service proftpd restart
 	 		checking_errors $?
 	 	else
-	 		echo -e "	${BWHITE}* Fine, nothing will be installed !${NC}"
+	 		echo -e " ${BWHITE}* Fine, nothing will be installed !${NC}"
 		fi
 	else
 		echo -e "	${BWHITE}* FTP Server already installed !${NC}"
 		if (whiptail --title "FTP Server" --yesno "Do you want to reconfigure your FTP Server ?" 7 50) then
 			FTPSERVERNAME=$(whiptail --title "FTPServer Name" --inputbox \
 			"Please enter a name for your FTP Server :" 7 50 "SeedBox" 3>&1 1>&2 2>&3)
-			echo -e "	${BWHITE}* Creating configuration file...${NC}"
+			echo -e " ${BWHITE}* Creating configuration file...${NC}"
 			mv "$PROFTPDFOLDER$PROFTPDCONFFILE" "$PROFTPDFOLDER$PROFTPDCONFFILE.bak"
 	 	 	cat "$BASEPROFTPDFILE" >> "$PROFTPDFOLDER$PROFTPDCONFFILE"
-	 	 	sed -i -e "s/ServerName\ \"Debian\"/$FTPSERVERNAME/g" "$PROFTPDFOLDER$PROFTPDCONFFILE"
+	 	 	sed -i -e "s/ServerName\ \"Debian\"/ServerName\ \"$FTPSERVERNAME\"/g" "$PROFTPDFOLDER$PROFTPDCONFFILE"
 	 	 	checking_errors $?
-	 	 	echo -e "	${BWHITE}* Restarting service...${NC}"
+	 	 	echo -e " ${BWHITE}* Restarting service...${NC}"
 	 	 	service proftpd restart
 	 		checking_errors $?
 	 	fi
