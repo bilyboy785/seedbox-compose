5f28127c6fc2dfec3894bf5cc45de36bf28d2950
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-19 18:11:41 +0200

    Bug fix

--- includes/functions.sh
+++ includes/functions.sh
@@ -11,6 +11,7 @@ function under_developpment() {
 function check_domain() {
 	if (whiptail --title "Domain access" --yesno "Are you sure your DNS entries are correctly configured ? We can test them now ;)" 10 90) then
 		TESTDOMAIN=$1
+		echo ""
 		echo -e "${BLUE}### CHECKING DNS ENTRIES ###${NC}"
 		echo -e " ${BWHITE}* Ping $TESTDOMAIN...${NC}"
 		ping -c 1 $TESTDOMAIN | grep "$IPADDRESS" > /dev/null
