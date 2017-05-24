60c0d2f3ccfcd46f2d9aea7f7950c6b86ee8c8ad
Martin Bouillaud <bouillaud.martin@gmail.com>
2017-05-24 14:12:04 +0200

    Move functions from build file

--- build.sh
+++ build.sh
@@ -9,47 +9,7 @@ if [ $USER = "root" ] ; then
 	script_option
 	case $SCRIPT in
 		"INSTALL")
-	    	if [[ ! -d "/etc/seedboxcompose/" ]]; then
-	    		clear
-		  		echo -e "${BLUE}##########################################${NC}"
-		  		echo -e "${BLUE}###    INSTALLING SEEDBOX-COMPOSE      ###${NC}"
-		  		echo -e "${BLUE}##########################################${NC}"
-				conf_dir
-				## Install base packages
-				install_base_packages
-			  ## Checking system version
-				checking_system
-				## Check for docker on system
-				install_docker
-				## Installing Nginx
-				install_nginx
-				## Installing ZSH
-				install_zsh
-				## Check for LetsEncrypt packages on system
-				install_letsencrypt
-				## Defines parameters for dockers : password, domains and replace it in docker-compose file
-				define_parameters
-				## Choose wich services install
-				choose_services
-				## Update docker-compose file
-				install_services
-				## Generate dockers apps running in background
-				docker_compose
-				## Create reverse proxy for each apps
-				create_reverse
-				## Ask user to instal FTP
-				install_ftp_server
-				## Resuming seedbox-compose installation
-				resume_seedbox
-				## Backup dockers app Configuration
-				backup_docker_conf
-				#schedule_backup_seedboxe
-				schedule_backup_seedbox
-	    	else
-				clear
-				echo -e " ${RED}--> Seedbox-Compose already installed !${NC}"
-	    		script_option
-	    	fi
+	    	first_install
 	  	;;
 		"ADDUSER")
 	    	add_user_htpasswd
--- includes/functions.sh
+++ includes/functions.sh
@@ -605,6 +605,50 @@ function generate_ssl_cert() {
 	fi
 }
 
+function first_install() {
+	if [[ ! -d "/etc/seedboxcompose/" ]]; then
+	    clear
+		echo -e "${BLUE}##########################################${NC}"
+		echo -e "${BLUE}###    INSTALLING SEEDBOX-COMPOSE      ###${NC}"
+		echo -e "${BLUE}##########################################${NC}"
+		conf_dir
+		## Install base packages
+		install_base_packages
+		## Checking system version
+		checking_system
+		## Check for docker on system
+		install_docker
+		## Installing Nginx
+		install_nginx
+		## Installing ZSH
+		install_zsh
+		## Check for LetsEncrypt packages on system
+		install_letsencrypt
+		## Defines parameters for dockers : password, domains and replace it in docker-compose file
+		define_parameters
+		## Choose wich services install
+		choose_services
+		## Update docker-compose file
+		install_services
+		## Generate dockers apps running in background
+		docker_compose
+		## Create reverse proxy for each apps
+		create_reverse
+		## Ask user to instal FTP
+		install_ftp_server
+		## Resuming seedbox-compose installation
+		resume_seedbox
+		## Backup dockers app Configuration
+		backup_docker_conf
+		#schedule_backup_seedboxe
+		schedule_backup_seedbox
+	else
+		clear
+		echo -e " ${RED}--> Seedbox-Compose already installed !${NC}"
+	    script_option
+	fi
+}
+
 function manage_users() {
 	echo -e "${BLUE}##########################################${NC}"
 	echo -e "${BLUE}###             MANAGE USERS           ###${NC}"
